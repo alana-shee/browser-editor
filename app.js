ecd8e6046b8ac85246cf30c82a85046074ed7cf3
Alana <alana.shee@gmail.com>
2017-12-11 18:49:11 -0500

    Added autosave to local storage

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
+// default text
 document.getElementById('heading').innerHTML = localStorage['title'] || 'This be the title...';
-document.getElementById('content').innerHTML = localStorage['text'] || '... and this be the text, you space monkey you.'
+document.getElementById('content').innerHTML = localStorage['text'] || '... and this be the text, you space monkey you.';
+
+// autosave
+setInterval(function(){
+	localStorage['title'] = document.getElementById('heading').innerHTML;
+	localStorage['text'] = document.getElementById('content').innerHTML;
+}, 1000);

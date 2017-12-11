d7233239b65af1ffaaf0d9192eedab074a508e5d
Alana <alana.shee@gmail.com>
2017-12-11 18:45:04 -0500

    Added default text

---
+++ app.js
@@ -0,0 +1,2 @@
+document.getElementById('heading').innerHTML = localStorage['title'] || 'This be the title...';
+document.getElementById('content').innerHTML = localStorage['text'] || '... and this be the text, you space monkey you.'
